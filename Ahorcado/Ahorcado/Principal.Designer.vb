a827bc033fd95a37a1c3380c2a5873107a9516a2
Feagurth <Feagurth@gmail.com>
2014-01-09 19:17:34 +0100

    Juego del Ahorcado - En Desarrollo
    Acabado la parte principal, quedan detallitos por acabar

---
+++ Ahorcado/Ahorcado/Principal.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Principal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picImagen = New System.Windows.Forms.PictureBox()
+        Me.lblPalabraDescubierta = New System.Windows.Forms.Label()
+        Me.txtIntroduceLetra = New System.Windows.Forms.TextBox()
+        Me.btnAñadir = New System.Windows.Forms.Button()
+        Me.lblEstado = New System.Windows.Forms.Label()
+        Me.lstListaLetras = New System.Windows.Forms.ListBox()
+        Me.Navegador = New System.Windows.Forms.WebBrowser()
+        CType(Me.picImagen, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picImagen
+        '
+        Me.picImagen.Image = Global.WindowsApplication1.My.Resources.Resources._00
+        Me.picImagen.Location = New System.Drawing.Point(12, 58)
+        Me.picImagen.Name = "picImagen"
+        Me.picImagen.Size = New System.Drawing.Size(310, 283)
+        Me.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picImagen.TabIndex = 0
+        Me.picImagen.TabStop = False
+        '
+        'lblPalabraDescubierta
+        '
+        Me.lblPalabraDescubierta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPalabraDescubierta.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPalabraDescubierta.Location = New System.Drawing.Point(12, 16)
+        Me.lblPalabraDescubierta.Name = "lblPalabraDescubierta"
+        Me.lblPalabraDescubierta.Size = New System.Drawing.Size(310, 23)
+        Me.lblPalabraDescubierta.TabIndex = 1
+        Me.lblPalabraDescubierta.Text = "Label1"
+        Me.lblPalabraDescubierta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtIntroduceLetra
+        '
+        Me.txtIntroduceLetra.Location = New System.Drawing.Point(12, 364)
+        Me.txtIntroduceLetra.Name = "txtIntroduceLetra"
+        Me.txtIntroduceLetra.Size = New System.Drawing.Size(137, 20)
+        Me.txtIntroduceLetra.TabIndex = 2
+        '
+        'btnAñadir
+        '
+        Me.btnAñadir.Location = New System.Drawing.Point(155, 362)
+        Me.btnAñadir.Name = "btnAñadir"
+        Me.btnAñadir.Size = New System.Drawing.Size(167, 23)
+        Me.btnAñadir.TabIndex = 3
+        Me.btnAñadir.Text = "Introducir"
+        Me.btnAñadir.UseVisualStyleBackColor = True
+        '
+        'lblEstado
+        '
+        Me.lblEstado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEstado.Location = New System.Drawing.Point(12, 398)
+        Me.lblEstado.Name = "lblEstado"
+        Me.lblEstado.Size = New System.Drawing.Size(310, 23)
+        Me.lblEstado.TabIndex = 4
+        Me.lblEstado.Text = "Label1"
+        Me.lblEstado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstListaLetras
+        '
+        Me.lstListaLetras.FormattingEnabled = True
+        Me.lstListaLetras.Location = New System.Drawing.Point(328, 58)
+        Me.lstListaLetras.Name = "lstListaLetras"
+        Me.lstListaLetras.Size = New System.Drawing.Size(41, 329)
+        Me.lstListaLetras.TabIndex = 5
+        '
+        'Navegador
+        '
+        Me.Navegador.Location = New System.Drawing.Point(445, 37)
+        Me.Navegador.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.Navegador.Name = "Navegador"
+        Me.Navegador.Size = New System.Drawing.Size(250, 250)
+        Me.Navegador.TabIndex = 6
+        '
+        'Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 392)
+        Me.Controls.Add(Me.Navegador)
+        Me.Controls.Add(Me.lstListaLetras)
+        Me.Controls.Add(Me.lblEstado)
+        Me.Controls.Add(Me.btnAñadir)
+        Me.Controls.Add(Me.txtIntroduceLetra)
+        Me.Controls.Add(Me.lblPalabraDescubierta)
+        Me.Controls.Add(Me.picImagen)
+        Me.MaximizeBox = False
+        Me.Name = "Principal"
+        Me.Text = "Ahorcado"
+        CType(Me.picImagen, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents picImagen As System.Windows.Forms.PictureBox
+    Friend WithEvents lblPalabraDescubierta As System.Windows.Forms.Label
+    Friend WithEvents txtIntroduceLetra As System.Windows.Forms.TextBox
+    Friend WithEvents btnAñadir As System.Windows.Forms.Button
+    Friend WithEvents lblEstado As System.Windows.Forms.Label
+    Friend WithEvents lstListaLetras As System.Windows.Forms.ListBox
+    Friend WithEvents Navegador As System.Windows.Forms.WebBrowser
+
+End Class
---
+++ Ahorcado/Ahorcado/Principal.vb
@@ -0,0 +1,158 @@
+﻿Public Class Principal
+
+    Enum Estado
+        GANO = 1
+        PIERDO = 2
+        CONTINUAR = 0
+    End Enum
+
+    Dim estadoJuego As Estado
+    Dim strPalabra As String
+    Dim strPalabraAdivinada As String = String.Empty
+    Dim intNumErrores As Integer = 0
+
+    Private Sub Principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Navegador.Navigate("http://www.palabrasque.com/palabra-aleatoria.php")
+
+        While Navegador.IsBusy
+            Debug.Print("Cargando...")
+        End While
+
+
+        strPalabra = BuscarPalabra()
+        lblPalabraDescubierta.Text = String.Empty
+
+        For Each letra As String In strPalabra
+            strPalabraAdivinada = strPalabraAdivinada + "_"
+        Next
+
+        For i As Integer = 0 To strPalabraAdivinada.Length
+            lblPalabraDescubierta.Text = lblPalabraDescubierta.Text + "_ "
+
+        Next
+
+
+
+
+        estadoJuego = Estado.CONTINUAR
+        lblEstado.Text = estadoJuego.ToString
+
+    End Sub
+
+    Private Function BuscarPalabra()
+        Dim resultado As String = "alcachofas"
+
+
+        Return resultado
+
+    End Function
+
+
+
+    Private Function ValidacionLetra(ByVal strCadena As String)
+        Dim resultado As Boolean = True
+
+        If strCadena.Length > 1 Then
+            resultado = False
+        End If
+
+        If IsNumeric(strCadena) Then
+            resultado = False
+        End If
+
+        If "abcdefghijklmnñopqrstuvwxyz".IndexOf(strCadena) = -1 Then
+            resultado = False
+        End If
+
+        Return resultado
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAñadir.Click
+        Dim strLetra As String
+        Dim intApoyo As Integer = 0
+
+        If txtIntroduceLetra.TextLength <> 0 Then
+            strLetra = txtIntroduceLetra.Text.ToLower
+
+            If lstListaLetras.Items.Contains(strLetra.ToUpper) = False Then
+
+                If ValidacionLetra(strLetra) Then
+
+                    lstListaLetras.Items.Add(strLetra.ToUpper)
+
+                    If strPalabra.IndexOf(strLetra) <> -1 Then
+
+                        Dim apoyo() As Char
+
+                        apoyo = strPalabraAdivinada.ToCharArray()
+
+                        lblPalabraDescubierta.Text = String.Empty
+                        strPalabraAdivinada = String.Empty
+
+                        For Each letras As String In strPalabra
+                            If letras = strLetra Then
+                                apoyo(intApoyo) = strLetra
+                            End If
+
+                            strPalabraAdivinada = strPalabraAdivinada + apoyo(intApoyo)
+                            lblPalabraDescubierta.Text = lblPalabraDescubierta.Text + apoyo(intApoyo) + " "
+
+                            intApoyo = intApoyo + 1
+                        Next
+
+                        If strPalabra = strPalabraAdivinada Then
+                            estadoJuego = Estado.GANO
+                        End If
+                    Else
+                        intNumErrores = intNumErrores + 1
+
+                        Select Case intNumErrores
+                            Case 1
+                                picImagen.Image = My.Resources._01
+                            Case 2
+                                picImagen.Image = My.Resources._02
+                            Case 3
+                                picImagen.Image = My.Resources._03
+                            Case 4
+                                picImagen.Image = My.Resources._04
+                            Case 5
+                                picImagen.Image = My.Resources._05
+                            Case 6
+                                picImagen.Image = My.Resources._06
+                                estadoJuego = Estado.PIERDO
+                        End Select
+
+                    End If
+                Else
+                    MsgBox("No es un caracter válido", MsgBoxStyle.Exclamation, "Atención")
+                End If
+            Else
+                MsgBox("Esa letra ya ha sido introducida", MsgBoxStyle.Exclamation, "Atención")
+            End If
+        End If
+
+        txtIntroduceLetra.Text = String.Empty
+        txtIntroduceLetra.Focus()
+
+        lblEstado.Text = estadoJuego.ToString
+
+        If estadoJuego = Estado.GANO Then
+            MsgBox("Ganaste", MsgBoxStyle.Information, "You Win")
+            lstListaLetras.Items.Clear()
+            picImagen.Image = My.Resources._00
+        End If
+        If estadoJuego = Estado.PIERDO Then
+            MsgBox("Perdiste", MsgBoxStyle.Critical, "You Lose")
+            lstListaLetras.Items.Clear()
+            picImagen.Image = My.Resources._00
+        End If
+
+
+    End Sub
+
+    Private Sub txtIntroduceLetra_KeyUp(sender As Object, e As KeyEventArgs) Handles txtIntroduceLetra.KeyUp
+        btnAñadir.Focus()
+    End Sub
+End Class
